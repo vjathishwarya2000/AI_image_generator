41e2760dfe5644a81dbf42da7517c20d62921a0b
vjathishwarya2000 <vsjathu2000@gmail.com>
2024-08-28 16:12:32 +0530

    frontend implemented

--- client/main.js
+++ client/main.js
@@ -1,14 +1,45 @@
 const generateBtn = document.querySelector(".generate-btn");
 const promptInput = document.querySelector(".prompt-input");
-const generatedImage = document.querySelector(".generated-image");
+const generatedImages = document.querySelectorAll(".generated-image");
+const downloadBtns = document.querySelectorAll(".download-btn");
 
-const generateImage = async () => {
-    const response = await fetch("http://localhost:4000/generateImage", {
-        method: "POST"
-    });
-
-    const data = await response.json();
-    console.log("data");
+const showNotification = (message) => {
+  alert(message);
 };
 
-generateImage();
+const generateImages = async () => {
+  const prompt = promptInput.value;
+
+  if (prompt) {
+    try {
+      generatedImages.forEach((img) => {
+        img.src = "images/generating.png";
+      });
+
+      const response = await fetch("http://localhost:4000/generateImages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt: prompt }),
+      });
+
+      const data = await response.json();
+      
+      generatedImages.forEach((img, index) => {
+        img.src = data.images[index];
+        downloadBtns[index].href = data.images[index];
+      });
+    } catch (error) {
+      console.log(error);
+      showNotification("The images could not be generated.");
+      generatedImages.forEach((img) => {
+        img.src = "images/image-placeholder.png";
+      });
+    }
+  } else {
+    showNotification("Please enter the prompt");
+  }
+};
+
+generateBtn.addEventListener("click", generateImages);
